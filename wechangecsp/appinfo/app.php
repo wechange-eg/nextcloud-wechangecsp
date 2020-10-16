05a44e677698d5f49fc41ad4a615aff39edddcd0
Dennis Brakhane <brakhane@gmail.com>
2020-10-16 18:07:52 +0200

    add wechangecsp

---
+++ wechangecsp/appinfo/app.php
@@ -0,0 +1,12 @@
+<?php
+
+$csp = new \OCP\AppFramework\Http\ContentSecurityPolicy();
+foreach(\OC::$server->query(\OCP\IConfig::class)->getSystemValue('wechange_csp_domains', []) as $domain) {
+
+  $csp->addAllowedScriptDomain($domain);
+  $csp->addAllowedImageDomain($domain);
+  $csp->addAllowedConnectDomain($domain);
+  $csp->addAllowedFormActionDomain($domain);
+}
+$cspManager = \OC::$server->query(\OCP\Security\IContentSecurityPolicyManager::class);
+$cspManager->addDefaultPolicy($csp);
