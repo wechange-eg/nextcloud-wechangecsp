606bdfe1985a5b86c0fcc0b5311c9bb21bea39bd
Dennis Brakhane <brakhane@gmail.com>
2021-02-23 20:17:53 +0100

    add frame policies

--- wechangecsp/appinfo/app.php
+++ wechangecsp/appinfo/app.php
@@ -7,6 +7,8 @@ foreach(\OC::$server->query(\OCP\IConfig::class)->getSystemValue('wechange_csp_d
   $csp->addAllowedImageDomain($domain);
   $csp->addAllowedConnectDomain($domain);
   $csp->addAllowedFormActionDomain($domain);
+  $csp->addAllowedFrameDomain($domain);
+  $csp->addAllowedFrameAncestorDomain($domain);
 }
 $cspManager = \OC::$server->query(\OCP\Security\IContentSecurityPolicyManager::class);
 $cspManager->addDefaultPolicy($csp);
